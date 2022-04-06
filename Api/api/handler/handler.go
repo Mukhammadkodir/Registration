b8fe6f641c5ccff0a59e5e43825e4d19731f5be4
mukhammadkodir <qodircodermmi@gmail.com>
2022-04-06 20:38:56 +0500

    some

--- Api/api/handler/handler.go
+++ Api/api/handler/handler.go
@@ -32,35 +32,3 @@ func New(c *HandlerV1Config) *handlerV1 {
 		inMemoryStorage: c.InMemoryStorage,
 	}
 }
-
-// func GetClaims(h *handlerV1, c *gin.Context) jwt.MapClaims {
-// 	var (
-// 		ErrUnauthorized = errors.New("unauthorized")
-// 		claims          jwt.MapClaims
-// 		err             error
-// 	)
-
-// 	Authorization.Token = c.GetHeader("Authorization")
-// 	if c.Request.Header.Get("Authorization") == "" {
-// 		c.JSON(http.StatusUnauthorized, models.ResponseError{
-// 			Error: models.InternalServerError{
-// 				Message: "Unauthorized request",
-// 			},
-// 		})
-// 		h.log.Error("Unauthorized request: ", logger.Error(ErrUnauthorized))
-// 		return nil
-// 	}
-
-// 	h.jwtHandler.Token = Authorization.Token
-// 	claims, err = h.jwtHandler.ExtractClaims()
-// 	if err != nil {
-// 		c.JSON(http.StatusUnauthorized, models.ResponseError{
-// 			Error: models.InternalServerError{
-// 				Message: "Unauthorized request",
-// 			},
-// 		})
-// 		h.log.Error("Unauthorized request: ", logger.Error(ErrUnauthorized))
-// 		return nil
-// 	}
-// 	return claims
-// }
